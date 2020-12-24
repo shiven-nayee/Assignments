3130bc90900c37e52b1bc398fda86a5d9ab2e01d
shiven-nayee <shiven.nayee@gmail.com>
2020-12-23 21:39:27 -0500

    Completed question 2

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,28 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  // Split into array
+  let words = title.split(" ");
+
+  let output = [];
+
+  // console.log(words)
+
+  for(i = 0; i < words.length; i++) {
+    // Seperate each word into letters
+    let split = words[i].split("");
+    
+    // Change first letter to uppercase
+    split[0] = split[0].toUpperCase();
+
+    // Join and push into array.
+    output.push(split.join(""));
+    // console.log(split)
+  };
+  
+  return output.join(" ");
 }
+
+titleCaseEdit("there hello")
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
