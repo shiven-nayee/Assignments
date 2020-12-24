879ab197fee96442ec7510948be021ee4880b286
shiven-nayee <shiven.nayee@gmail.com>
2020-12-23 22:45:14 -0500

    Completed question 5

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,13 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let countOfBools = 0;
+  
+  for(i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === 'boolean') {
+      countOfBools++;
+    }
+  }
+  return countOfBools;
 }
 
 // Do not edit this line;
