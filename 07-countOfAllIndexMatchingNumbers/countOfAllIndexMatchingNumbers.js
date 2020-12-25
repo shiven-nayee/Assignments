a52306dd9eaac8d0f8f0b3a5713e800e094833ae
shiven-nayee <shiven.nayee@gmail.com>
2020-12-24 23:00:53 -0500

    Completed question 8

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,11 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  for(i = 0; i < nums.length; i++) {
+    if(nums[i] == i) {
+      count ++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
