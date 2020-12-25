3d69329474c1f1324269b25a6a69716fc13bb41f
shiven-nayee <shiven.nayee@gmail.com>
2020-12-24 22:33:11 -0500

    Completed question 6

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let count = 0;
+  for(i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === "boolean" || typeof arr[i] === "string") {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
