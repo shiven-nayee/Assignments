8380aeae188e92c0001ea24f85e1b5373c21fcfa
shiven-nayee <shiven.nayee@gmail.com>
2020-12-28 09:15:43 -0500

    Completed question 10

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let sum = 0;
+  for(i = 0; i < nums.length; i++) {
+    if(nums[i]%2 == 1) {
+      sum++;
+    }
+  }
+  return sum;
 }
 
 // Do not edit this line;
