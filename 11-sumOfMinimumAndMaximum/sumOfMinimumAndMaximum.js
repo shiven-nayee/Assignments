c012200765af57c8c1d998736bc7528d6481ce1a
shiven-nayee <shiven.nayee@gmail.com>
2020-12-28 09:28:21 -0500

    Completed question 12

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let lowest = nums[0];
+  let highest = nums[0];
+  for(i = 0; i < nums.length; i++) {
+    if(nums[i] < lowest) {
+      lowest = nums[i];
+    }
+    if(nums[i] > highest) {
+      highest = nums[i];
+    }
+  }
+  return (highest + lowest);
 }
 
 // Do not edit this line;
