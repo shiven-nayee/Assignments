47d1ea2aeb3101679432f593f5d5b31eb7d45f57
shiven-nayee <shiven.nayee@gmail.com>
2020-12-28 09:42:35 -0500

    Completed question 14

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,16 @@
-function isPalindrome(word) {
-  // Insert code here;
+function reverseWord(word) {
+  let reversedWord = "";
+  for(i = word.length-1; i >= 0; i--) {
+    reversedWord += word.charAt(i);
+  }
+  return reversedWord;
 }
+
+function isPalindrome(word) {
+  return (reverseWord(word) === word);
+}
+
+reverseWord("Hello");
 
 // Do not edit this line;
 module.exports = isPalindrome;
