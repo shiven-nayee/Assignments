35314d14dffc6362fa66ba90f55ff328aa06c53e
shiven-nayee <shiven.nayee@gmail.com>
2020-12-28 09:36:03 -0500

    Completed question 13

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  let output = [];
+  for(i = start; i <= end; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+      output.push("FizzBuzz");
+    } else if(i % 5 == 0) {
+      output.push("Buzz");
+    } else if(i % 3 == 0 ) {
+      output.push("Fizz");
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
