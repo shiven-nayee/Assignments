9cceeef530c8afaf4724850db20740ccb8c2cfed
shiven-nayee <shiven.nayee@gmail.com>
2021-01-03 19:36:41 -0500

    Completed question 15

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,28 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let output = {};
+  for(i = 0; i < word.length; i++) {
+    let character = word.charAt(i);
+    if(output[character]) {
+      output[character]++;
+    } else {
+      output[character] = 1;
+    }
+  }
+  return output;
 }
+
+// Optimal solution using maps
+// let map = new Map();
+//   for(i = 0; i < word.length; i++) {
+//     if(map.get(word[i]) == undefined) {
+//       map.set(word[i], 1);
+//     } else {
+//       map.set(word[i], map.get(word[i]) + 1)
+//     }
+//   }
+
+frequencyCounter("hello")
 
 // Do not edit this line;
 module.exports = frequencyCounter;
