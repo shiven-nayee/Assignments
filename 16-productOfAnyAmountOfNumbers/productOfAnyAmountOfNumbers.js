3868ddc64d32c8b3c4e3d8bef71d43011479c572
shiven-nayee <shiven.nayee@gmail.com>
2021-01-03 20:40:21 -0500

    Completed question 17

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let product = 1;
+  args.forEach(element => {
+    product = product*element;
+  });
+  return product;
 }
 
 // Do not edit this line;
