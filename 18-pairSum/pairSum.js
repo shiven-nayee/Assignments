c0aeab7d969d468c3c3c193f51fc77878b6b8883
shiven-nayee <shiven.nayee@gmail.com>
2021-01-04 15:29:51 -0500

    Completed question 19

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,16 @@
 function pairSum(nums, target) {
   // Insert code here;
+  if(nums.length <= 1) {
+    throw(Error);
+  }
+  for(i = 0; i < nums.length; i++) {
+    for(j = 0; j < nums.length; j++) {
+      if((nums[i] + nums[j]) == target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
