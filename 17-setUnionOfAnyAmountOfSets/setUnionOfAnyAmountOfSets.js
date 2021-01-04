72996557204d8ef3cad669b5adcab9c750ac1426
shiven-nayee <shiven.nayee@gmail.com>
2021-01-04 15:14:54 -0500

    Completed question 18

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,18 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let output = new Set();
+  args.forEach(element => {
+    element.forEach(num => {
+      if(!(output.has(num))) {
+        output.add(num);
+      }
+    })
+  });
+
+  return output;
 }
+
+setUnionOfAnyAmountOfSets()
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
